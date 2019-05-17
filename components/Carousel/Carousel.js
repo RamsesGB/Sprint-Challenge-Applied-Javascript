9be05e8742259b91cb1ff17ee4c3abe46606786f
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-05-17 14:00:08 -0400

    final submission

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,16 +1,27 @@
 class Carousel {
     constructor(carouselEle){
         this.carouselEle = carouselEle;
-        this.leftArrow = document.querySelector('.left-button');
-        this.rightArrow = document.querySelector('.right-button');
-    }
 
+        this.imgData = this.carouselEle.dataset.tab;
+        console.log(this.imgData)
+        //reference left arrow and create click handler
+        this.leftArrow = document.querySelector('.left-button');
+        this.leftArrow.addEventListener('click', () => this.leftClick());
+        //reference right arrow and create click handler
+        this.rightArrow = document.querySelector('.right-button');
+        this.leftArrow.addEventListener('click', () => this.rightClick());
+    }
+    leftClick(){
+
+    }
+    rightClick(){
+
+    }
 }
 
 let carousel = document.querySelector('.carousel');
-let images = document.querySelectorAll('.carousel-img');
 
-// new Carousel(carousel);
+new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
